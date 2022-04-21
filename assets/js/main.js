fffc6e445cec8d3c0dc8aaacec9f632ba11455f2
Anoop B <16367697+anoop-b@users.noreply.github.com>
2022-04-21 13:31:41 +0530

    feat: new post template with Form

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,60 @@ $(function() {
         })
     });
 });
+/**
+ * Helper function for POSTing data as JSON with fetch.
+ *
+ * @param {Object} options
+ * @param {string} options.url - URL to POST data to
+ * @param {FormData} options.formData - `FormData` instance
+ * @return {Object} - Response body from URL that was POSTed to
+ */
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: formDataJsonString,
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
+}
+
+/**
+ * Event handler for a form submit event.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+ *
+ * @param {SubmitEvent} event
+ */
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        await postFormDataAsJson({ url, formData });
+
+        alert("Email Sent! Please check your inbox");
+        form.reset();
+    } catch (error) {
+        alert("Something went wrong, Please try again later");
+    }
+}
+
+const bsnlForm = document.getElementById("bsnl-form");
+bsnlForm.addEventListener("submit", handleFormSubmit);
